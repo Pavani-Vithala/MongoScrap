954cb0f20cd2b14707ccabe8e61df77956395175
Pavani Vithala <pavani.vithala18@gmail.com>
2019-04-04 22:20:15 -0400

    Clean code

--- models/Article.js
+++ models/Article.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
+// Using the Schema constructor, create a new ArticleSchema object
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
       if (data) {
         for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
+          // Display the approp information on the page
           var site = "https://www.nytimes.com"
           var articlediv = $("<div>");
           var save = $("<button>");
@@ -20,7 +20,6 @@ $(document).ready(function () {
           articlediv.css("background-color", "beige");
           articlediv.css("margin", "10px");
           articlediv.css("padding", "10px");
-          // save.css("float","right");
           $(".background-image").append(articlediv);
           articlediv.html('<a href =' + site + data[i].link + '><h4>' + data[i].title + data[i].summary + '</h4></a>');
           articlediv.append(save);
@@ -50,7 +49,7 @@ $(document).ready(function () {
       if (data) {
         $("#" + id).parent().hide();
       }
-      // window.location()
+      
     });
   });
 
@@ -66,50 +65,69 @@ $(document).ready(function () {
     });
   });
 
-
-
-
   // Handle the Save Articles list
   $("#Saved").on("click", function () {
+    //Get API call to get the data scraped
     $.ajax({
       url: "/api/SavedArticles/",
       type: 'GET',
     }).then(function (data) {
       $("articlediv").remove();
+      //Check if any data returned and if yes, execute the below 
       if (data) {
-        console.log("Entered clear articles loop");
-        console.log("Successfully removed articles div");
+        //For each record returned,execute the below loop
         for (var i = 0; i < data.length; i++) {
-          // Display the apropos information on the page
           console.log("Displaying saved article" + i);
+          //store the site url to a variable so that it can be appended with the link from the response object
           var site = "https://www.nytimes.com"
+          //Create a div to display the scrapped articles
           var articleSavediv = $("<div>");
+          //Create a button for deleting the articles
           var Delete = $("<button>");
-          var addNote = $("<buton>");
+          //Create a button to add Notes for the article
+          var addNote = $("<button>");
+          // Assign a class to the Delete button
           Delete.addClass("btn btn-danger Delete");
+          //Assign a class to the addNote button
           addNote.addClass("btn btn-primary Notes");
+          //Add padding to the delete button
           Delete.attr("padding", "10px");
+          //Add padding to the addNote button
           addNote.attr("padding", "10px");
+          //Add the text to be displayed on the delete button
           Delete.text("DELETE ARTICLE");
+          //Add the text to be displayed on the add note button
           addNote.text("Add Notes");
+          //Add margin of 20px to the delete button
           Delete.css("margin", "20px");
+          //Add a margin of 20px to the addNote button
           addNote.css("margin", "20px");
+          //Assign an id attribute to the div whcih is the index of the response data object
           articleSavediv.attr("id", i);
+          //Add class called Savedarticles to the div as we assigned the id of the div as i in the previous step
           $("#" + i).addClass("Savedarticles");
+          //Set the background color of the div
           articleSavediv.css("background-color", "beige");
+          //Set margin and padding of the div
           articleSavediv.css("margin", "10px");
           articleSavediv.css("padding", "10px");
-          // save.css("float","right");
+          //Append the div to the class called .background-image which displays the newyork times image
           $(".background-image").append(articleSavediv);
+          //Set the html of div as the various attributes of the data object returned from server like link, title,summary 
           articleSavediv.html('<a href =' + site + data[i].link + '><h3>' + data[i].title + data[i].summary + '</h3></a>');
+          //Append the delete button to the div
           articleSavediv.append(Delete);
+          //Append the addNote button to the div
           articleSavediv.append(addNote);
+          //Set the colorof the div to black
           articleSavediv.css("color", "black");
+          //set the id of the delete button as the id attribute of the article returned in the response object so that this can be used to pass the id to delete the article with id from the db
           Delete.attr("id", data[i]._id);
+           //set the id of the addNote button as the id attribute of the article returned in the response object so that this can be used to add notes to the same article with id as the addNote button
           addNote.attr("id", data[i]._id);
 
         }
-
+        //If no data is returned, display text as "No Saved Articles. Scrap new articles"
       } else {
         var text = "No Saved articles.Scrap new articles";
         var articleh2 = $("<h2>");
@@ -130,14 +148,13 @@ $(document).ready(function () {
       if (data) {
         $("#" + id).parent().hide();
       }
-      // window.location()
+    
     });
   });
 //handle modal close button
 $(document).on("click", ".btn.btn-secondary", function () {
   console.log("Entered the close modal function:");
   $(".AddedNotes").html("");
-  //$("#NoteText").html("");
   $("#confirmBox").hide();
 });
 
@@ -158,7 +175,6 @@ $(document).on("click", ".btn.btn-secondary", function () {
           for(var i =0;i<data.length;i++)
           {
             $(".AddedNotes").append("<br>" + data[i].body );
-           // $(".AddedNotes").append(data[i].body);
           }
          } else{
             var msg = "No Notes yet for the article";
